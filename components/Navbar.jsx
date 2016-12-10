e9369597d8c21a89a261d07677167476bde05b85
Nyapal <nyapal14@gmail.com>
2016-12-10 09:27:15 -0500

    added navbar, installed react-router

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import {Link} from 'react-router'
 
-class Navbar extends React.Component {
-   render() {
-      return (
-        <div>
-           <h1>USER NAVBAR</h1>
-        </div>
-     );
+const Navbar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/page1'>Page 1</Link>
+      </div>
+    )
   }
-}
+})
 
 export default Navbar;
--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory,IndexRoute} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import App from './App.jsx';
 import Home from './components/Home.jsx'
+import Page1 from './components/page1.jsx'
 
 ReactDOM.render(
   <Router history={hashHistory}>
    <Route path="/" component={App} >
      <IndexRoute component={Home} />
+     <Route path="/page1" component={Page1}/>
   </Route>
 </Router>
 , document.getElementById('app'));
