03b2d0c53a91bd7ac4128202ad273925f06fbd1d
jondubya86 <jonathanwu86@gmail.com>
2016-12-10 03:34:37 -0500

    working react with webpack-dev

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+           <Navbar/>
+            Hello World!!!
+         </div>
+      );
+   }
+}
+
+export default App;
